b7f4e6ee51f4556918b5501e5355cbcb9f536cce
okonomiyakki <parkji12532@naver.com>
2024-12-08 04:39:24 +0900

    feat(config): 환경변수 설정

---
+++ src/config/config.service.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+class ConfigService {
+  constructor() {
+    this.envConfig = process.env;
+  }
+
+  _get(key, DEFAULT = null) {
+    return this.envConfig[key] || DEFAULT;
+  }
+
+  getServerConfig() {
+    return {
+      server: {
+        host: this._get('SERVER_HOST'),
+        ws: { namespace: this._get('SERVER_WS_NAMESPACE') },
+      },
+    };
+  }
+
+  getLcuApiConfig() {
+    return {
+      lcuApi: { currentSummoner: this._get('LCU_API_CURRENT_SUMMONER') },
+    };
+  }
+
+  getRiotApiConfig() {
+    return {
+      riotApi: {
+        asia: {
+          host: this._get('RIOT_API_ASIA_HOST'),
+          account: {
+            v1: {
+              riotId: this._get('RIOT_API_ASIA_ACCOUNT_V1_RIOT_ID'),
+            },
+          },
+        },
+        appKey: this._get('RIOT_API_APP_KEY'),
+      },
+    };
+  }
+}
+
+module.exports = ConfigService;
